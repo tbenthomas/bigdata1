177aed1bb41e6d23de467f9df9bf5e0a1939c355
Your name here <your_email@example.com>
2020-02-29 13:52:24 -0500

    Improved Exception handling

--- src/apicall.py
+++ src/apicall.py
@@ -2,17 +2,26 @@ from sodapy import Socrata
 import json
 
 def api_call(inputs: dict):
+    #create api client
     client=Socrata(
         "data.cityofnewyork.us",
         inputs['APP_KEY'])
+        
+    #either store api calls to file or print to terminal
+    if "output" in inputs.keys():
+        try:
+            with open(inputs['output'], 'w') as fout:
+                for i in range(inputs['num_pages']):
+                    data_get=client.get("nc67-uf89",limit=inputs['page_size'],offset=inputs['page_size']*i)
+                    json.dump(data_get,fout)
+        except FileNotFoundError as err:
+            print("\nPlease enter a valid file name with extension, or leave out --output to print to terminal")
+    else:
+        for i in range(inputs['num_pages']):
+            print(client.get("nc67-uf89",limit=inputs['page_size'],offset=inputs['page_size']*i))
                 
-    try:
-        with open(inputs['output'], 'w') as fout:
-            for i in range(inputs['num_pages']):
-                data_get=client.get("nc67-uf89",limit=inputs['page_size'],offset=inputs['page_size']*i)
-                json.dump(data_get,fout)
-    except KeyError:
-        print("Error: Please enter a file name with extension '.json'")
+        print("No output file specified, outputting to terminal")
+    
     
 
     
--- src/input.py
+++ src/input.py
@@ -1,25 +1,40 @@
 import getopt,sys,os
 
 def  get_inputs() -> dict:
- options,remainder = getopt.getopt(sys.argv[1:],"p:n:o",['page_size=','num_pages=','output='])
-
+ #instantiate dict that will be returned
  result={}
- for opt,arg in options:
-     if opt=="--page_size":
-         try:
-             result['page_size']=int(arg)
-         except:
-             print("Error: Page_size must be an integer value")
-     if opt=="--num_pages":
-         try:
-            result['num_pages'] =int(arg)
-         except:
-             print('Error: num_pages must be an integer value')
-     if opt=="--output":
-        if arg[-4:]=='json':
-            result['output'] = arg
+ 
+ #get terminal inputs
+ try:
+    options,remainder = getopt.getopt(sys.argv[1:],"p:n:o",['page_size=','num_pages=','output='])
+ except getopt.GetoptError as err:
+    print(str(err))
 
- result['APP_KEY']=os.environ['APP_KEY']
+ #build dict that will be returned
+ try:
+    for opt,arg in options:
+        if opt=="--page_size":
+            try:
+                result['page_size']=int(arg)
+            except:
+                print("Error: Page_size must be an integer value")
+        if opt=="--num_pages":
+            try:
+                result['num_pages'] =int(arg)
+            except:
+                print('Error: num_pages must be an integer value')
+        if opt=="--output":
+            result['output'] = arg
+ except UnboundLocalError as err:
+     print("\nInvalid argument\nvalid arguments: --page_size,--num_pages,--output\n\n")
+     sys.exit(1)
+
+ #capture app key from enviroment
+ try:
+    result['APP_KEY']=os.environ['APP_KEY']
+ except KeyError:
+     print("\nPlease set your socrata api app token in an environment variable called APP_KEY")
+     sys.exit(1)
  
  return result
 
