65e42df1fdecd9c7e78c430e211513b231597f41
=tbenthomas <your_email@example.com>
2020-02-29 22:16:47 -0500

    Made argument --page_num optional

--- src/apicall.py
+++ src/apicall.py
@@ -1,5 +1,5 @@
 from sodapy import Socrata
-import json
+import json,sys
 
 def api_call(inputs: dict):
     #create api client
@@ -14,13 +14,29 @@ def api_call(inputs: dict):
                 for i in range(inputs['num_pages']):
                     data_get=client.get("nc67-uf89",limit=inputs['page_size'],offset=inputs['page_size']*i)
                     json.dump(data_get,fout)
+                
         except FileNotFoundError as err:
             print("\nPlease enter a valid file name with extension, or leave out --output to print to terminal")
+            sys.exit(1)
+        except KeyError as err:
+            print(err)
+            print("\n check arguments please")
+            sys.exit(1)
+    elif "num_pages" in inputs.keys():
+        try: 
+            for i in range(inputs['num_pages']):
+                print(f"\n\n PAGE NUMBER {i+1}\n\n")
+                print(client.get("nc67-uf89",limit=inputs['page_size'],offset=inputs['page_size']*i))
+        except KeyError as err:
+            print(err)
+            print("\n check arguments please")  
+            sys.exit(1)  
     else:
-        for i in range(inputs['num_pages']):
-            print(client.get("nc67-uf89",limit=inputs['page_size'],offset=inputs['page_size']*i))
-                
-        print("No output file specified, outputting to terminal")
+        data = client.get("nc67-uf89")
+        print(client.get("nc67-uf89"))
+        print(f"\n\nNumber of pages = {int(len(data)/inputs['page_size'])}")
+        print(f"Number of rows = {len(data)}")
+    print("No output file specified, outputting to terminal")
     
     
 
