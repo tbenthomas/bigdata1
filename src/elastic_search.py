ca4018ff0fc7e1a0cd61bdf8793abab0757072a6
=tbenthomas <your_email@example.com>
2020-03-07 16:56:23 -0500

    docker-compose added

---
+++ src/elastic_search.py
@@ -0,0 +1,21 @@
+from elasticsearch import Elasticsearch
+
+def prep_data(data: list) -> dict:
+    result=[]
+    for record in data:
+        temp =dict(record)
+        entry = {'index': temp['plate'],
+                  'doc_type':'ticket',
+                  'body': temp
+                  }
+        result.append(entry)
+    return result
+        
+def push_data(es: Elasticsearch,data: list):
+
+    records=prep_data(data)
+
+    for record in records:    
+        res = es.index(index=record['index'],doc_type=record['doc_type'],body=record['body'])
+        print(res['result'])
+    
